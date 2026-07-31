2af6ecbe034a5dd94d54e0e4f8442c85978d8915
Tony Fonda <tonyfonda-eng@users.noreply.github.com>
2026-07-31 18:27:44 +0100

    feat: add additional SEC EDGAR scrapers for tender offers, proxy statements, and merger filings

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,6 +1,6 @@
 from .businesswire import BusinessWireScraper
 from .globenewswire import GlobeNewswireScraper
-from .edgar import EdgarScraper, Edgar13DScraper, EdgarForm10Scraper
+from .edgar import EdgarScraper, Edgar13DScraper, EdgarForm10Scraper, EdgarTenderOfferScraper, Edgar14D9Scraper, EdgarMergerProxyScraper, EdgarDefinitiveProxyScraper, EdgarS4Scraper
 from .edgar_items import EdgarItemScraper
 from .prnewswire import PRNewsWireScraper
 from .kedm import KEDMScraper
@@ -14,6 +14,11 @@ SCRAPER_REGISTRY = {
     "SEC Edgar": EdgarScraper,
     "SEC EDGAR - Schedule 13D (Activism)": Edgar13DScraper,
     "SEC EDGAR - Form 10 (Spin-Offs)": EdgarForm10Scraper,
+    "SEC EDGAR - Tender Offers (SC TO)": EdgarTenderOfferScraper,
+    "SEC EDGAR - Board Rec (14D-9)": Edgar14D9Scraper,
+    "SEC EDGAR - Merger Proxy (PREM14A)": EdgarMergerProxyScraper,
+    "SEC EDGAR - Definitive Proxy (DEFM14A)": EdgarDefinitiveProxyScraper,
+    "SEC EDGAR - S-4 (Stock Mergers)": EdgarS4Scraper,
     "SEC Edgar 8-K Items": EdgarItemScraper,
     "PR Newswire": PRNewsWireScraper,
     "KEDM": KEDMScraper,
